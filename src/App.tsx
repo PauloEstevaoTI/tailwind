5e9512b3d2eb89a9ce4a1abe37288c7a3b6e1a9f
Bombeta <paullo_jf@hotmail.com>
2024-03-13 13:41:38 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className=" container h-screen w-screen 
+    mx-auto flex items-center justify-center ">
+      <p>hey</p>
+    </div>
+  );
+}
+
+export default App;
